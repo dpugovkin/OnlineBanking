0c4c7824fdaa96bda747feca8e27bd25c2e40120
Dmytro Pugovkin <dmytro.pugovkin@gmail.com>
2019-04-19 17:01:28 +0200

    [new] initial commit

---
+++ src/main/java/com/pugovkin/onlinebanking/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.pugovkin.onlinebanking;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OnlinebankingApplication.class);
+    }
+
+}
